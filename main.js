5f9c379754234deae2b7dd75440d61ddb2ba35a9
Özgür Yildirim <ozguryenihayat@gmail.com>
2022-10-21 22:03:21 +0200

    interactive play

--- main.js
+++ main.js
@@ -3,16 +3,30 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
+let attempts = 8;
+
 const words = Array.of("python", "java", "swift", "javascript");
 
 const correct = words[Math.floor(Math.random() * words.length)]
 
+let clue = new Array(correct.length).fill('-');
+
 console.log("H A N G M A N");
 
-const answer = input("Guess the word: ");
-
-if (answer == correct) {
-    console.log("You survived!");
-} else {
-    console.log("You lost!")
+for (let i = 0; i < attempts; i++) {
+    console.log();
+    console.log(clue.join(""));
+    const letter = input("Input a letter: ")
+    if (correct.includes(letter)) {
+        for (let j = 0; j < correct.length; j++) {
+            if (correct.charAt(j) == letter) {
+                clue[j] = correct[j];
+            }
+        }
+    } else {
+        console.log("That letter doesn't appear in the word.")
+    }
 }
+
+console.log();
+console.log(("Thanks for playing!"))
